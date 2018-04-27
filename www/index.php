c03329b0b0fb25716beecc36020bb5f39c220d4c
yaurau <rama7777.ravtovtch@yandex.ru>
2018-04-27 16:31:07 +0300

    Editing the code

--- www/index.php
+++ www/index.php
@@ -2,10 +2,12 @@
 require_once __DIR__ .'/../vendor/autoload.php';
 $loader = new Twig_Loader_Filesystem(__DIR__ .'/public/templates');
 $twig = new Twig_Environment($loader, array(
-    'cache' => '/cache/compilation_cache',
+    'cache' => __DIR__ . '/cache/compilation_cache',
 ));
-
-echo $twig->render('layout.html.twig', array('name' => 'Yaurau Framework'));
+echo $twig->render('layout.html.twig', array(
+    'name' => 'Yaurau Framework',
+    'creater' => 'Yaurau Framework',
+));
 //error_reporting(E_ALL);
 //use Application\Models\View;
 /*require __DIR__ . '/../vendor/autoload.php';
