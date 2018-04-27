1008976468cf9b73dd3ba37e2d43f9a5d97bb30f
Rauvtovich Yauhen <rama7777@bk.ru>
2018-04-27 19:58:52 +0300

    Update Mail.php

--- www/classes/Mail.php
+++ www/classes/Mail.php
@@ -9,12 +9,12 @@ class Mail {
         $mail->isSMTP();                                      // Set mailer to use SMTP
         $mail->Host = 'smtp.yandex.ru';  // Specify main and backup SMTP servers
         $mail->SMTPAuth = true;                               // Enable SMTP authentication
-        $mail->Username = 'rama7777.ravtovtch@yandex.ru';                 // SMTP username
-        $mail->Password = 'rama777710';                           // SMTP password
+        $mail->Username = '';                 // SMTP username
+        $mail->Password = '';                           // SMTP password
         $mail->SMTPSecure = 'SSL';                            // Enable TLS encryption, `ssl` also accepted
         $mail->Port = 25;
-        $mail->setFrom('rama7777.ravtovtch@yandex.ru', 'Dr. dred');
-        $mail->addAddress('rama7777@mail.ru');
+        $mail->setFrom('rama7777ch@yandex.ru', 'Dr. dred');
+        $mail->addAddress('rama777@mail.ru');
         $mail->Subject = 'News';
         $mail->Body ='News add';
         if(!$mail->send()) {
@@ -29,9 +29,3 @@ class Mail {
 
 }
 
-/**
- * Created by PhpStorm.
- * User: User
- * Date: 15.05.2017
- * Time: 18:43
- */
