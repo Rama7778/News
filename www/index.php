1802d90d177cdaadbc3ac26e74c879b61b38fc6e
yaurau <rama7777@bk.ru>
2018-05-11 17:26:34 +0300

    Updating the code

--- www/index.php
+++ www/index.php
@@ -7,6 +7,13 @@ $twig = new Twig_Environment($loader, array(
 echo $twig->render('layout.html.twig', array(
     'name' => 'Yaurau Framework',
     'creater' => 'Yaurau Framework',
+    'title' => 'Yaurau Framework',
+    'what_we' => 'what we',
+    'we_offer_our_customers' => 'we offer our customers',
+    'Our' => 'Our',
+    'service' => 'service',
+    'Projects' => 'Projects'
+
 ));
 ?>
 
