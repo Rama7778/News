4eb620247c93bfb38b57ffb8ca978ca71c4169ff
yaurau <rama7777.ravtovtch@yandex.ru>
2018-05-12 09:17:45 +0300

    Editing the code

--- www/view/Admin/distrue.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once __DIR__ . '/../models/SiteController.phproller.php';
-   echo newsAdd();
-?>
-<br>
-<a href="http://news/">Вернуться на главную</a>
-<br>
-<a href="http://news/views/addfail.php">Вернуться обратно</a>
