7277a63857f3cd15cecabcf86bc06282fa040688
yaurau <rama7777@bk.ru>
2018-07-27 08:40:49 +0300

    Code update

--- www/models/SiteValues.php
+++ www/models/SiteValues.php
@@ -5,8 +5,7 @@ class SiteValues
 {
     static public function setBaseValues()
     {
-        $array = ['name' => 'Yaurau Framework',
-            'creater' => 'Yaurau Framework',
+        $array = [
             'title' => 'Yaurau Framework',
             'what_we' => 'what we',
             'we_offer_our_customers' => 'we offer our customers',
@@ -14,13 +13,10 @@ class SiteValues
             'we_offer_our_service' => 'we offer our service',
             'projects' => 'Projects',
             'title_banner_first_1' => 'title banner first',
-            'title_banner_second_1' => 'title banner',
             'text_banner_second_1' => 'text banner',
             'title_banner_first_2' => 'title banner first',
-            'title_banner_second_2' => 'title banner',
             'text_banner_second_2' => 'text banner',
             'title_banner_first_3' => 'title banner first',
-            'title_banner_second_3' => 'title banner',
             'text_banner_second_3' => 'text banner',
             'icon_title_offer_1' => 'icon_title_offer_1',
             'icon_text_offer_1' => 'icon_text_offer_1',
@@ -37,7 +33,9 @@ class SiteValues
             'icon_title_service_4' => 'icon_title_service_4',
             'icon_text_service_4' => 'icon_title_service_4',
             'telefon' => '222-0002-222222',
-            'map' => '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2352.855861207814!2d27.51865981591274!3d53.86321098009061!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46dbd0417696dcf7%3A0x409d60ef596fa55a!2z0YPQu9C40YbQsCDQmtCw0LfQuNC90YbQsCAyNCwg0JzQuNC90YHQug!5e0!3m2!1sru!2sby!4v1526110553741" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>',
+            'map' => '<iframe src="https://yandex.ru/map-widget/v1/?um=constructor%
+3Adc1b0285c7e2c088a3b27e4da7541a6d60b72b2081464087537df146afb6b21f&amp;source=constructor"
+ width="100%" height="400" frameborder="0"></iframe>',
             'mail' => 'mailto:info@example.com'];
         foreach ($array as $key=>$value){
             $val = new Values();
